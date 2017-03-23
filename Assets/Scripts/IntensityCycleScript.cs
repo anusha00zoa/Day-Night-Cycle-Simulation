caa7dac7dded7f17e51eb49f400bb6dfa1d1ba70
Anusha Singh <anusha00zoa@gmail.com>
2017-03-22 21:31:01 -0500

    Committing asset files, project settings

---
+++ Assets/Scripts/IntensityCycleScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntensityCycleScript : MonoBehaviour 
+{
+	public Vector3 dayRotateSpeed;
+	[HideInInspector]
+	public Vector3 nightRotateSpeed;
+	[HideInInspector]
+	public Vector3 rSpeed;
+
+	Vector3 drs, nrs;
+
+	public float maxIntensity = 3f;
+	public float minIntensity = 0f;
+	public float minPoint = -0.2f;
+
+	//[HideInInspector]
+	public Light mainLight;
+
+	public Scrollbar scroll;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//mainLight = transform;
+
+		nightRotateSpeed = dayRotateSpeed * 5;
+		drs = dayRotateSpeed;
+		nrs = nightRotateSpeed;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float tRange = 1 - minPoint;
+		float dot = Mathf.Clamp01 ((Vector3.Dot (mainLight.transform.forward, Vector3.down) - minPoint) / tRange);
+		float i = ((maxIntensity - minIntensity) * dot) + minIntensity;
+
+		mainLight.intensity = i;
+
+		if (scroll.value >= 1f)
+			scroll.value = 0f;
+
+		if (scroll.value >= 0.5f)
+			rSpeed = nightRotateSpeed;
+		else
+			rSpeed = dayRotateSpeed;
+		
+		transform.Rotate (rSpeed * Time.deltaTime);
+		scroll.value += rSpeed.x * Time.deltaTime / 360;
+	}
+
+	public void ResetRotateSpeed()
+	{
+		dayRotateSpeed = drs;
+		nightRotateSpeed = nrs;
+	}
+}
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+	public Button playPauseButton; 
+	public Sprite playImage;
+	public Sprite pauseImage;
+	Sprite currentImage;
+
+//	public Scrollbar timelineScroll;
+	Scrollbar timeline;
+
+	public Toggle toggleGI;
+
+	bool isPlaying =  true;
+	bool GIOn = true;
+
+	GameObject lightObject;
+	Light sunLight;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playPauseButton.GetComponent<Image>().sprite = pauseImage;
+		currentImage = pauseImage;
+
+		lightObject = GameObject.FindWithTag ("GILayer");
+		timeline = lightObject.GetComponent<IntensityCycleScript> ().scroll;
+		sunLight = lightObject.GetComponent<IntensityCycleScript> ().mainLight;
+
+		if (timeline == null)
+			print ("Cannot find scrollbar.");
+		else
+			print (timeline.name);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float markerX = Input.GetAxis ("Mouse X");
+		float scaleFactor = 5f;
+
+		if (!isPlaying)
+		{
+			lightObject.GetComponent<IntensityCycleScript> ().dayRotateSpeed = new Vector3(markerX * scaleFactor, 0, 0);	//timelineScroll.enabled = false;
+			lightObject.GetComponent<IntensityCycleScript>().nightRotateSpeed = lightObject.GetComponent<IntensityCycleScript>().dayRotateSpeed * 4;
+		}
+		else
+			lightObject.GetComponent<IntensityCycleScript> ().ResetRotateSpeed ();	//timelineScroll.enabled = true;
+	}
+
+	public void GIDisabled()
+	{
+		if (GIOn) 
+		{
+			toggleGI.GetComponentInChildren<Text> ().text = "GI Off";
+			sunLight.enabled = false;
+		} 
+		else 
+		{
+			toggleGI.GetComponentInChildren<Text> ().text = "GI On";
+			sunLight.enabled = true;
+		}
+		
+		GIOn = !GIOn;
+	}
+
+
+	public void PlayPause()
+	{
+		if (isPlaying)
+			currentImage = playImage;
+		else
+			currentImage = pauseImage;
+
+		playPauseButton.GetComponent<Image>().sprite = currentImage;
+		isPlaying = !isPlaying;
+	}
+}
